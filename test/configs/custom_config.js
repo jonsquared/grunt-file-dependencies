7154cbabe25db992e8196d37e3b55812dd6ca276
grohm33 <grohm33@gmail.com>
2014-12-17 20:37:30 -0500

    committing tests for custom regex configs

--- test/configs/custom_config.js
+++ test/configs/custom_config.js
@@ -1,10 +1,15 @@
 module.exports = function(grunt) {
     grunt.config('file_dependencies.custom_config', {
       files: {
-      	src: ['test/fixtures/custom_config/*.js']
+      	src: [
+    		'test/fixtures/custom_config/Test.js',  
+    		'test/fixtures/custom_config/ClassA.js'
+      	]
       },
       options: {
-	      outputProperty: 'customtask.files.src'
+	      outputProperty: 'customtask.files.src',
+	      extractDefinesRegex: /create\s*\(\s*['"]([^'"]+)['"]/g,
+	      extractRequiresRegex: /use\s*\(\s*['"]([^'"]+)['"]/g,
       }
     });
 }
--- test/custom_config_test.js
+++ test/custom_config_test.js
@@ -2,12 +2,16 @@
 
 var grunt = require('grunt');
 
-exports.single_define_test = {
+exports.custom_config_test = {
   custom_options: function(test) {
     test.expect(1);
 
     var actual = grunt.config.get('customtask.files.src');
-    var expected = ['test/fixtures/custom_config/ClassA.js'];
+    var options = grunt.config.get('file_dependencies').custom_config.options;
+    var expected = [
+    	'test/fixtures/custom_config/ClassA.js',
+    	'test/fixtures/custom_config/Test.js'    	
+    	];
     test.deepEqual(actual, expected, 'generates file array in specified property');
     test.done();
   }
